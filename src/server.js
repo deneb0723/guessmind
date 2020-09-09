9fd3301bf68a52e52c7d891de4266162f54238f3
denev <65989857+deneb0723@users.noreply.github.com>
2020-09-10 03:23:17 +0900

    chat

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 import express from "express";
-import { join } from "path";
+import {
+  join
+} from "path";
 import socketIO from "socket.io";
+import logger from "morgan";
 
 const PORT = 4000;
 const app = express();
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
+app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
 app.get("/", (req, res) => res.render("home"));
 
@@ -14,4 +18,20 @@ const handleListening = () =>
 
 const server = app.listen(PORT, handleListening);
 
-const io = socketIO(server);
+const io = socketIO.listen(server);
+
+io.on("connection", socket => {
+  socket.on("newMessage", ({
+    message
+  }) => {
+    socket.broadcast.emit("messageNotif", {
+      message,
+      nickname: socket.nickname || "Anon"
+    })
+  });
+  socket.on("setNickname", ({
+    nickname
+  }) => {
+    socket.nickname = nickname;
+  });
+});
