f251a74a7e009d464ceb6413e0b14b169c3756c3
denev <65989857+deneb0723@users.noreply.github.com>
2020-09-28 07:50:33 +0900

    로그인 알림 기능, 스타일링

---
+++ src/socketController.js
@@ -0,0 +1,14 @@
+import events from "./events";
+
+const socketController = (socket) => {
+    socket.on(events.setNickname, ({
+        nickname
+    }) => {
+        socket.nickname = nickname;
+        socket.broadcast.emit(events.newUser, {
+            nickname
+        });
+    });
+};
+
+export default socketController;
